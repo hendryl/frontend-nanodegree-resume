14f9962c785e01ed0816c677835b0966d21be036
Hendry <hendry@icehousecorp.com>
2015-05-06 17:55:05 +0700

    Create header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append("Hendry");
+var formattedName = HTMLheaderName.replace("%data%", "Hendry");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
