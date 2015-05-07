04e444cad718f218c5f855d07ee95d72752c8ead
Hendry <hendry@icehousecorp.com>
2015-05-07 17:49:30 +0700

    change all divs to black

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,59 @@
-var formattedName = HTMLheaderName.replace("%data%", "Hendry");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+var bio = {
+  name: "Hendry",
+  role: "Web developer",
+  welcomeMessage: "Welcome to my page",
+  skills: ["Sleeping", "Eating"],
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+  contacts: {
+    mobile: "08888888",
+    email: "hendrylee_1993@yahoo.com",
+    github: "hendryl"
+  },
+  blog: "-",
+  location: "Jakarta"
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Ice House",
+      "title": "Intern Engineer",
+      "location": "Jakarta",
+      "dates": "2015 - now"
+    }  
+  ]
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Binus University",
+      "city": "Jakarta",
+      "major": "Computer Science",
+      "graduate": "2016"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title":"Javascript",
+      "school":"Udacity",
+      "dates":"2015",
+      "url":"------"
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title":"WORLD DOMINATION",
+      "dates":"EVERYDAY",
+      "description":"LALALA LILILI LOLOLOLOLO"
+    },
+    {
+      "title":"Test",
+      "dates":"Every week",
+      "description":"This is a test project"
+    }
+  ]
+}
